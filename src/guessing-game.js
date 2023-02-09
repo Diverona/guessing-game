601f0a73ca85d6d946113afcb67a3de8e1f1fdc2
Kateryna Tonapetian <kattonapetian@gmail.com>
2023-02-10 00:06:05 +0200

    solve the task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,22 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.minValue = min;
+    this.maxValue = max;
+  }
 
-    }
+  guess() {
+    return Math.round((this.maxValue + this.minValue) / 2);
+  }
 
-    guess() {
+  lower() {
+    return (this.maxValue = Math.round((this.maxValue + this.minValue) / 2));
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    return (this.minValue = Math.round((this.maxValue + this.minValue) / 2));
+  }
 }
 
 module.exports = GuessingGame;
